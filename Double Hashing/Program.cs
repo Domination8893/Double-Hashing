1c5ac3c415ac29cbcad00dc186a082d942ffa44d
pc <pc@DESKTOP-1A76K0D>
2023-04-12 11:06:22 +0200

    Add project files.

---
+++ Double Hashing/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+// Define the hash table data structure
+class HashTable :
+    // Constructor to initialize the hash table with a given size
+    function __init__(size):
+        this.size = size
+        this.table = new Array[size] // Array of buckets to store key-value pairs
+
+    // Define a hash function to compute the primary hash
+    function hash(key):
+        // ... Hash function implementation ...
+        // Returns an index in the range [0, size-1]
+
+    // Define a secondary hash function to compute the secondary hash for double hashing
+    function secondaryHash(key):
+        // ... Secondary hash function implementation ...
+        // Returns an integer that is relatively prime to size
+
+    // Define a function to insert a key-value pair into the hash table
+    function insert(key, value):
+        index = hash(key) // Compute the primary hash
+
+        if table[index] is empty: // If the bucket is empty
+            table[index] = (key, value) // Store the key-value pair in the bucket
+        else: // If the bucket is not empty, resolve collision using double hashing
+            step = secondaryHash(key) // Compute the secondary hash step
+            i = 1 // Initialize the step counter
+
+            // Loop until an empty bucket is found
+            while table[(index + i * step) % size] is not empty:
+                i = i + 1 // Move to the next step
+            end while
+
+            // Store the key-value pair in the empty bucket
+            table[(index + i * step) % size] = (key, value)
+
+    // Define a function to retrieve the value associated with a given key
+    function retrieve(key):
+        index = hash(key) // Compute the primary hash
+
+        if table[index] is not empty:
+            if table[index].key is equal to key: // If the key matches
+    return table[index].value // Return the corresponding value
+            else: // If the key doesn't match, use double hashing to find the correct bucket
+                step = secondaryHash(key) // Compute the secondary hash step
+                i = 1 // Initialize the step counter
+
+                // Loop until the key is found or an empty bucket is encountered
+                while table[(index + i * step) % size] is not empty:
+                    if table[(index + i * step) % size].key is equal to key: // If the key matches
+    return table[(index + i * step) % size].value // Return the corresponding value
+                    i = i + 1 // Move to the next step
+                end while
+        }
+
+        return null // Key not found
+    end function
+
+    // Define a function to remove a key-value pair from the hash table
+    function remove(key):
+        index = hash(key) // Compute the primary hash
+
+        if table[index] is not empty:
+            if table[index].key is equal to key: // If the key matches
+    table[index] = null // Remove the key-value pair from the bucket
+                return // Return successfully
+            else: // If the key doesn't match, use double hashing to find the correct bucket
+                step = secondaryHash(key) // Compute the secondary hash step
+                i = 1 // Initialize the step counter
+
+                // Loop until the key is found or an empty bucket is encountered
+                while table[(index + i * step) % size] is not empty:
+                    if table[(index + i * step) % size].key is equal to key: // If the key matches
+    table[(index + i * step) % size] = null // Remove the
+                    // key-value pair from the bucket
+                    return // Return successfully
+                    i = i + 1 // Move to the next step
+                end while
+        }
+
+        return // Key not found
+    end function
+*/
